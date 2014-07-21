13edcbad96d1c28791cee5f3876dbc6c2473f2fb
Ayelet Dahan <ayelet.dahan@healarium.com>
2014-07-21 11:43:18 +0300

    added angular to event editing

--- event/static/event/js/addEvent.js
+++ event/static/event/js/addEvent.js
@@ -1,107 +1,184 @@
-$(function() {
-	function AdditiveList(options) {
-		this.options = options || {};
-		
-		var	thisClass = this,
-			$itemsList = $(options.list),
-			$itemsInputList = $itemsList.find('.item_inputs'),
-			$lineTemplate = $itemsList.find('.item');
-		this.showItemsList = function() {
+(function() {
 
-			// get the list of items
-			thisClass.getList().done(function(data) {
-				var items = data.results;
-				$itemsList.show();
+	var editEventApp = angular.module('editevent', ['ngRoute'])
+					.config(function ($routeProvider) {
+						$routeProvider
+							.when('/:eventId?', {
+								templateUrl: '/static/event/views/edit.html',
+								controller: 'EditEventController'
+							});
+						});
 
-				// an empty list
-				if ($.isArray(items) && items.length !== 0) {
-					// build the items list
-					for (var i in items) {
-						thisClass.setItem(items[i].email);
-					}
-				}
-
-				// line template should have a plus sign and enabled
-				$lineTemplate.clone().prependTo($itemsInputList)
-							.find('input').prop('disabled', false).addClass('list_add').end()
-							.find('span').removeClass('glyphicon-minus').addClass('glyphicon-plus');
-
-				$lineTemplate.hide();
-				thisClass.initListActions();
-			});
-
-		};
-		this.getList = function() {
-			return $.get(this.options.url);
-		};
-		this.setItem = function(item) {
-			$lineTemplate.clone(true, true).appendTo($itemsInputList)
-						.find('input').val(item).end() // set the value
-						.find('.glyphicon-minus').click(this.removeItem).end() // bind remove
-						.show();
-		};
-		this.initListActions = function() {
-			$itemsList.find('.glyphicon-plus').click(this.addItem).end()
-						.find('.list_add').keypress(this.watchItemInput);
-		};
-		this.removeItem = function() {
-			$(this).parent().remove();
-		};
-		this.addItem = function() {
-			var $this = $(this);
-			var $input = $this.is("input") ? $this : $this.parent().find('input');
-			thisClass.setItem($input.val());
-			$input.val('');
-		};
-		this.watchItemInput = function(e) {
-			if (e.keyCode === 13)
-				thisClass.addItem.call(this, e);
-		};
-	}
-	var	initDate = function() {
-			$('#id_date').datepicker();
-		},
-		initAddressSearch = function() {
-			var searchBox = new google.maps.places.SearchBox(
-					document.getElementById('id_place')
-				);
-		},
-		advanceStep = function() {
-			switch ($('form').attr('data-step')) {
-				case 'step_0':
-					showGuests();
-					break;
-				case 'step_1':
-					showProducts();
-					break;
+	editEventApp.factory('EditEvent', function ($http) {
+		return {
+			fetchEvent: function(eventid) {
+				// if no event id given, return an empty object
+				if (!eventid)
+					return {};
+				// get the event for editing
+				return $http.get('/event/' + eventid);
+			},
+			saveEvent: function(eventid, eventdata) {
+				if (eventid)
+					return $http.put('/event/' + eventid, eventdata);
+				else
+					return $http.post('/event/add/', eventdata);
 			}
-			var products = new AdditiveList({
-				list: '#party_list'
-			});
-			$('button[name=step_2').click(advanceStep);
-		},
-		showGuests = function() {
-			var guests = new AdditiveList({
-				list: '#guest_list',
-				url: '/event/guests/'
-			});
-			guests.showItemsList();
-			// advance step
-			$('form').attr('data-step', 'step_1');
-		},
-		showProducts = function() {
-			var products = new AdditiveList({
-				list: '#party_list',
-				url: '/event/products'
-			});
-			products.showItemsList();
-			$('form').attr('data-step', 'step_2');
-		},
-		initialize = function() {
-			initDate();
-			initAddressSearch();
-			$('button[name=advance_step').click(advanceStep);
 		};
+	});
 
-	initialize();
-});
+	editEventApp.factory('Guests', function ($http, ofEvent) {
+		return {
+			/**
+			 * Add a new guest to the guests list of the event
+			 * @param {Guest} data The data of the guest
+			 */
+			addGuest: function(data) {
+				return $http.post('/guest/' + ofEvent, data);
+			},
+			/**
+			 * Remove a guest from the event's guest list
+			 * @param  {String} id The Guest's id
+			 * @return {Promise}    A promise object to use when action is executed
+			 */
+			removeGuest: function(id) {
+				return $http.delete('/guest/' + ofEvent + '/' + id);
+			}
+		};
+	});
+
+	editEventApp.factory('Products', function ($http, ofEvent, ofGuest) {
+		return {
+			/**
+			 * Add a new product to the guests list of the event
+			 * @param {Guest} data The data of the guest
+			 */
+			addGuest: function(data) {
+				return $http.post('/product/' + ofEvent + '/' + ofGuest, data);
+			},
+			/**
+			 * Remove a product from the event's guest list
+			 * @param  {String} id The Guest's id
+			 * @return {Promise}    A promise object to use when action is executed
+			 */
+			removeGuest: function(id) {
+				return $http.delete('/product/' + ofEvent + '/' + ofGuest + '/' + id);
+			}
+		};
+	});
+
+	editEventApp.controller('EditEventController', ['$scope', '$routeParams', 'EditEvent', function ($scope, $routeParams, EditEvent) {
+		var eventid = $routeParams.eventId; // event ID, if requested
+		if (eventId) {
+			EditEvent.fetchEvent(eventid).success(function(eventData) {
+				$scope.event = eventData;
+			});
+		}
+	}]);
+
+	// function AdditiveList(options) {
+	// 	this.options = options || {};
+		
+	// 	var	thisClass = this,
+	// 		$itemsList = $(options.list),
+	// 		$itemsInputList = $itemsList.find('.item_inputs'),
+	// 		$lineTemplate = $itemsList.find('.item');
+	// 	this.showItemsList = function() {
+
+	// 		// get the list of items
+	// 		thisClass.getList().done(function(data) {
+	// 			var items = data.results;
+	// 			$itemsList.show();
+
+	// 			// an empty list
+	// 			if ($.isArray(items) && items.length !== 0) {
+	// 				// build the items list
+	// 				for (var i in items) {
+	// 					thisClass.setItem(items[i].email);
+	// 				}
+	// 			}
+
+	// 			// line template should have a plus sign and enabled
+	// 			$lineTemplate.clone().prependTo($itemsInputList)
+	// 						.find('input').prop('disabled', false).addClass('list_add').end()
+	// 						.find('span').removeClass('glyphicon-minus').addClass('glyphicon-plus');
+
+	// 			$lineTemplate.hide();
+	// 			thisClass.initListActions();
+	// 		});
+
+	// 	};
+	// 	this.getList = function() {
+	// 		return $.get(this.options.url);
+	// 	};
+	// 	this.setItem = function(item) {
+	// 		$lineTemplate.clone(true, true).appendTo($itemsInputList)
+	// 					.find('input').val(item).end() // set the value
+	// 					.find('.glyphicon-minus').click(this.removeItem).end() // bind remove
+	// 					.show();
+	// 	};
+	// 	this.initListActions = function() {
+	// 		$itemsList.find('.glyphicon-plus').click(this.addItem).end()
+	// 					.find('.list_add').keypress(this.watchItemInput);
+	// 	};
+	// 	this.removeItem = function() {
+	// 		$(this).parent().remove();
+	// 	};
+	// 	this.addItem = function() {
+	// 		var $this = $(this);
+	// 		var $input = $this.is("input") ? $this : $this.parent().find('input');
+	// 		thisClass.setItem($input.val());
+	// 		$input.val('');
+	// 	};
+	// 	this.watchItemInput = function(e) {
+	// 		if (e.keyCode === 13)
+	// 			thisClass.addItem.call(this, e);
+	// 	};
+	// }
+	// var	initDate = function() {
+	// 		$('#id_date').datepicker();
+	// 	},
+	// 	initAddressSearch = function() {
+	// 		var searchBox = new google.maps.places.SearchBox(
+	// 				document.getElementById('id_place')
+	// 			);
+	// 	},
+	// 	advanceStep = function() {
+	// 		switch ($('form').attr('data-step')) {
+	// 			case 'step_0':
+	// 				showGuests();
+	// 				break;
+	// 			case 'step_1':
+	// 				showProducts();
+	// 				break;
+	// 		}
+	// 		var products = new AdditiveList({
+	// 			list: '#party_list'
+	// 		});
+	// 		$('button[name=step_2').click(advanceStep);
+	// 	},
+	// 	showGuests = function() {
+	// 		var guests = new AdditiveList({
+	// 			list: '#guest_list',
+	// 			url: '/event/guests/'
+	// 		});
+	// 		guests.showItemsList();
+	// 		// advance step
+	// 		$('form').attr('data-step', 'step_1');
+	// 	},
+	// 	showProducts = function() {
+	// 		var products = new AdditiveList({
+	// 			list: '#party_list',
+	// 			url: '/event/products'
+	// 		});
+	// 		products.showItemsList();
+	// 		$('form').attr('data-step', 'step_2');
+	// 	},
+	// 	initialize = function() {
+	// 		initDate();
+	// 		initAddressSearch();
+	// 		$('button[name=advance_step').click(advanceStep);
+	// 	};
+
+	// initialize();
+})();
